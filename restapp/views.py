5549961b62dd54ce3ffa842be85b22a01a2890b0
abbaskaf <142451585+abbaskaf@users.noreply.github.com>
2023-12-29 09:52:06 +0330

    Update views.py

--- restapp/views.py
+++ restapp/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import Ser
-
+#hello
 
 @api_view(['GET', 'POST'])
 def hello_world(request):
